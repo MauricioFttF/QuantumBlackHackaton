d8d8cbe6d83899485fed724696034e8d90d570a5
mauricio <mauricio.foletto@usp.br>
2026-08-19 22:07:32 -0300

    fix: melhora aparencia do front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import Chat from "./Chat";
 
 function App() {
-  return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>AI Forum Chat</h1>
-      <Chat />
-    </div>
-  );
+  return <Chat />;
 }
 
 export default App;
